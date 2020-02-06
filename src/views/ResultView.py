b32f4f0cb5dc7bc49bbd6c2f6cf60c349d315c37
Duncan Skinner <duncanskinner94@gmail.com>
2020-02-06 15:23:06 +0000

    imminent changes!

--- src/views/ResultView.py
+++ src/views/ResultView.py
@@ -11,27 +11,27 @@ game_schema = GameSchema()
 @result_api.route('/<int:game_id>/new', methods=['POST'])
 @Auth.auth_required
 def create(game_id):
-      current_user_id = Auth.current_user_id()
-      req_data = request.get_json()
-      winner = PlayerModel.get_one_player(req_data['winner_id'])
-      loser = PlayerModel.get_one_player(req_data['loser_id'])
-      data = result_schema.load(req_data)
-      result = ResultModel.get_result_by_game(game_id)
-      if result:
+        current_user_id = Auth.current_user_id()
+        req_data = request.get_json()
+        winner = PlayerModel.get_one_player(req_data['winner_id'])
+        loser = PlayerModel.get_one_player(req_data['loser_id'])
+        data = result_schema.load(req_data)
+        result = ResultModel.get_result_by_game(game_id)
+        if result:
           message = {'error': 'Result already provided'}
           return custom_response(message, 400)
-      game = GameModel.get_one_game(game_id)
-      if game.status != "confirmed":
-          message = {'error': 'Game needs to be confirmed to add a result'}
-          return custom_response(message, 400)
-      if game.organiser_id == current_user_id:
-          result = ResultModel(data)
-          winner.update_winner_rank_points()
-          loser.update_loser_rank_points()
-          result.save()
-          return custom_response(data, 201)
-      message = {'error': 'You can only add a result if you are the organiser.'}
-      return custom_response(message, 400)
+        game = GameModel.get_one_game(game_id)
+    #   if game.status != "confirmed":
+    #       message = {'error': 'Game needs to be confirmed to add a result'}
+    #       return custom_response(message, 400)
+    #   if game.organiser_id == current_user_id:
+        result = ResultModel(data)
+        # winner.update_winner_rank_points()
+        # loser.update_loser_rank_points()
+        result.save()
+        return custom_response(data, 201)
+    #   message = {'error': 'You can only add a result if you are the organiser.'}
+    #   return custom_response(message, 400)
 
 @result_api.route('/', methods=['GET'])
 @Auth.auth_required
